2f2a98568d8ae06efe8cf654534dc7a0a19b6690
Ian <user.emai>
2024-04-15 00:02:13 +0300

    Scope Lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Declare customerName in global scope
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+
+function setBestCustomer() {
+    // Declare bestCustomer in global scope
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    // Declare bestCustomer in global scope
+    bestCustomer = 'maybe bob';
+}
+
+// Declare leastFavoriteCustomer in global scope
+const leastFavoriteCustomer = 'someone';
+
+function changeLeastFavoriteCustomer() {
+    // This will throw an error because you cannot reassign a value to a constant
+    leastFavoriteCustomer = 'someone else';
+}
